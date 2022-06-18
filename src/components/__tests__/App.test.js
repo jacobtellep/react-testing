c20e5b30937d7ee63c93584b434f640600091acc
Jake Tellep <jj.tellep@gmail.com>
2022-06-18 00:01:37 -0400

    Left off on video 17. Implementing first test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,1 +1,10 @@
 import App from '../App';
+import ReactDOM from 'react-dom';
+
+it('shows a comment box', () => {
+  const div = document.create('div');
+
+  ReactDOM.render(<App />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});

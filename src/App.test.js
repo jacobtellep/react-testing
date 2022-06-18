3a6045bedc1b029e006d5400c8f03cb3eab99ffd
Jake Tellep <jj.tellep@gmail.com>
2022-06-17 23:42:03 -0400

    cleaning up unused files etc out of /src

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hi there', () => {
-  render(<App />);
-  const text = screen.getByText(/hi there/i);
-  expect(text).toBeInTheDocument();
-});
